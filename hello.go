4f87daad5edc7334ed699884fd16bf2b1ed8dd02
Nakama <nakama@00000126-MacBook-Pro.local>
2019-06-13 16:07:48 +0700

    add connection to postgres

--- hello.go
+++ hello.go
@@ -1,8 +1,19 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = ""
+	dbname   = "circleci_demo"
 )
 
 func print(w http.ResponseWriter, r *http.Request) {
@@ -15,6 +26,19 @@ func message(name string) string {
 }
 
 func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
 	http.HandleFunc("/", print)
 	http.ListenAndServe(":8080", nil)
 }
