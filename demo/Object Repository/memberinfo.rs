41c5b85fb00cced99beac5b1c2575645e112ea69
Nakama <nakama@00000126-MacBook-Pro.local>
2019-06-14 17:13:02 +0700

    add katalon dir

---
+++ demo/Object Repository/memberinfo.rs
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>memberinfo</name>
+   <tag></tag>
+   <elementGuidId>44a72eb5-c431-46b0-871e-f448b31dd082</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <followRedirects>false</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent></httpBodyContent>
+   <httpBodyType></httpBodyType>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>GET</restRequestMethod>
+   <restUrl>http://localhost:8080/member/${memberId}</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceFunction></soapServiceFunction>
+   <variables>
+      <defaultValue>1</defaultValue>
+      <description></description>
+      <id>c4ae790d-fc92-4df2-bf0f-0f19e389cf7a</id>
+      <masked>false</masked>
+      <name>memberId</name>
+   </variables>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
+</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
---
+++ demo/Test Suites/Get Member Data.ts
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Get Member Data</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>b4e6c2c5-40ad-4c49-bc72-22a0c9228f0e</testSuiteGuid>
+   <testCaseLink>
+      <guid>28c5774c-68f6-4723-b9b5-ef2313bf851f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API/Get Member Info/Success, given correct ID</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4a30c032-7ebc-4313-9af1-82d4fe81c3f6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API/Get Member Info/Fail, ID not given</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f26cebcf-4a26-4635-a5e6-f7f22e45c532</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API/Get Member Info/Fail, given wrong ID</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
