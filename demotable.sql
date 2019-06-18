30865173f8e00e30a0991b86f0103b14019393a0
Nakama <nakama@00000126-MacBook-Pro.local>
2019-06-18 10:24:29 +0700

    add dump data

---
+++ demotable.sql
@@ -0,0 +1,49 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 11.3
+-- Dumped by pg_dump version 11.3
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: member; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.member (
+    id integer,
+    name character varying(255),
+    email character varying(255)
+);
+
+
+ALTER TABLE public.member OWNER TO postgres;
+
+--
+-- Data for Name: member; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.member (id, name, email) FROM stdin;
+1	satu	satu@mail.com
+2	dua	dua@mail.com
+\.
+
+
+--
+-- PostgreSQL database dump complete
+--
+
